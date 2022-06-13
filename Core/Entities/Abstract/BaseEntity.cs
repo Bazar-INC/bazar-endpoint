2bfa1d052595054275e75a55e829ddea97839f51
Mike-Opanasiuk <iiwooltrapii@gmail.com>
2022-06-13 13:13:56 +0300

    Add data context

---
+++ Core/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Abstract;
+
+public abstract class BaseEntity : IEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
