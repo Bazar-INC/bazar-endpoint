22fcef83979ae42aa4a4f511e6445b83ddb407bc
Mike-Opanasiuk <iiwooltrapii@gmail.com>
2022-06-13 14:20:23 +0300

    Connect to local db and seed role

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,20 @@
+using Core.Entities;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services
+    .AddIdentity<UserEntity, RoleEntity>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +22,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSeed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
