f37e5f34eb2d1db982f6b73b7d1837443b720738
Mike Opanasiuk <iiwooltrapii@gmail.com>
2022-08-06 10:46:09 +0300

    Add cors origins

--- Web/Program.cs
+++ Web/Program.cs
@@ -88,7 +88,8 @@ try
     builder.Services.AddCors(options =>
     {
         options.AddDefaultPolicy(policy => policy
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("http://localhost:3000",
+                         "https://rozetka-clone.herokuapp.com")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
