74e0ad155f29c73dfa409db9e4dae2624057da5b
Mike Opanasiuk <iiwooltrapii@gmail.com>
2022-08-14 16:48:45 +0300

    Add category code to GET categories request

--- Application/Features/CategoryFeatures/Dtos/CategoryDto.cs
+++ Application/Features/CategoryFeatures/Dtos/CategoryDto.cs
@@ -1,13 +1,11 @@
-﻿
-using Application.Features.ProductFeatures.Dtos;
-
-namespace Application.Features.CategoryFeatures.Dtos;
+﻿namespace Application.Features.CategoryFeatures.Dtos;
 
 public class CategoryDto
 {
     public Guid Id { get; set; }
 
     public string? Name { get; set; }
+    public string? Code { get; set; }
 
     public ICollection<CategoryDto>? Children { get; set; }
 }
