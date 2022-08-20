f927b9eeac4f13b714f051c75c625ee67d49f8c7
Mike Opanasiuk <iiwooltrapii@gmail.com>
2022-08-20 11:27:49 +0300

    Add (Id) & (IsEdited) properties to feedback dto

--- Application/Features/FeedbackFeatures/Dtos/FeedbackResponseDto.cs
+++ Application/Features/FeedbackFeatures/Dtos/FeedbackResponseDto.cs
@@ -5,9 +5,11 @@ namespace Application.Features.FeedbackFeatures.Dtos;
 
 public class FeedbackResponseDto
 {
+    public Guid Id { get; set; }
     public string? CreatedAt { get; set; }
     public string? Text { get; set; }
     public int Stars { get; set; }
+    public bool IsEdited { get; set; }
     public virtual UserDto? Owner { get; set; }
     public virtual ICollection<FeedbackAnswerResponseDto> Answers { get; set; } = new List<FeedbackAnswerResponseDto>();
 }
